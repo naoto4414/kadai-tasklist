0b6dc34e8d593358ff09ffb8db68e50f8b1af407
EC2 Default User <ec2-user@ip-172-31-13-81.us-east-2.compute.internal>
2018-03-15 08:15:40 +0000

    tuika

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,7 +48,9 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   
   end
+   private
+
+  def task_params
+    params.require(:task).permit(:content )
 end
-   def task_params
-    params.require(:task).permit(:content, :status)
-  end
+end
